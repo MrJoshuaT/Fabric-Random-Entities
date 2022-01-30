511ea0298cbf2477fe12887b002b5edc0da5b786
MrJoshuaT <joshua.turner1@yahoo.com>
2022-01-30 22:27:38 +0000

    Temp remove random eyes textures renderer when iris is loaded due to class changes

--- src/main/java/com/mrjoshuat/fabricrandomentities/mixin/EyesFeatureRendererMixin.java
+++ src/main/java/com/mrjoshuat/fabricrandomentities/mixin/EyesFeatureRendererMixin.java
@@ -2,6 +2,7 @@ package com.mrjoshuat.fabricrandomentities.mixin;
 
 import com.mrjoshuat.fabricrandomentities.ClientMod;
 import com.mrjoshuat.fabricrandomentities.RandomEntitiesManager;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.model.Model;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.feature.EyesFeatureRenderer;
@@ -30,12 +31,18 @@ abstract class EyesFeatureRendererMixin<T extends Entity, M extends EntityModel<
         method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/Entity;FFFFFF)V",
         cancellable = true
     )
-    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, Entity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch,
+    public void renderEyesFeature(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, Entity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch,
                        CallbackInfo info) {
+        if (FabricLoader.getInstance().isModLoaded("iris")) {
+            return;
+        }
+
         // Legit ewwwwww noises
-        var layer = (RenderLayer.MultiPhase)getEyesTexture();
+        var layer = (RenderLayer.MultiPhase) getEyesTexture();
+
+        // TODO: if Iris is loaded this should be cast to an IrisRenderTypeWrapper?
         var phases = layer.getPhases();
-        var phaseAccessor = ((MultiPhaseParametersAccessor)(Object)phases);
+        var phaseAccessor = ((MultiPhaseParametersAccessor) (Object) phases);
         var id = phaseAccessor.getTexture().getId();
 
         if (id.isEmpty())
